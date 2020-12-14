938e10b8cf2cce380867ef9df97b661f69686641
Peter Murray <681306+peter-murray@users.noreply.github.com>
2020-12-14 16:45:53 +0000

    Adding end to end workflow

--- images/run.py
+++ images/run.py
@@ -30,10 +30,3 @@ matrix.SetImage(image.convert('RGB'))
 
 time.sleep(20)
 sys.exit(0)
-
-# try:
-    # print("Press CTRL-C to stop.")
-    # while True:
-    #     time.sleep(100)
-# except KeyboardInterrupt:
-    # sys.exit(0)
