5f0c137b938043a0ef0f616eb94bb8b9a92bb0a9
Peter Murray <681306+peter-murray@users.noreply.github.com>
2020-12-15 18:53:02 +0000

    Modifying font text drawing

--- text/run.py
+++ text/run.py
@@ -18,15 +18,16 @@ class RunText(SampleBase):
         pos = offscreen_canvas.width
         my_text = self.args.text
 
+        # Run for 20 seconds
         end_time = time.time() + 20
         while time.time() < end_time:
             offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+            len = graphics.DrawText(offscreen_canvas, font, pos, 60, textColor, my_text)
             pos -= 1
             if (pos + len < 0):
                 pos = offscreen_canvas.width
 
-            time.sleep(0.05)
+            time.sleep(0.02)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
