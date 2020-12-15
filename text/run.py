267b055977352ab761f821974c713657db60c988
Peter Murray <681306+peter-murray@users.noreply.github.com>
2020-12-15 18:57:56 +0000

    Positioning text

--- text/run.py
+++ text/run.py
@@ -22,7 +22,7 @@ class RunText(SampleBase):
         end_time = time.time() + 20
         while time.time() < end_time:
             offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 60, textColor, my_text)
+            len = graphics.DrawText(offscreen_canvas, font, pos, 40, textColor, my_text)
             pos -= 1
             if (pos + len < 0):
                 pos = offscreen_canvas.width
