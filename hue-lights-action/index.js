33049106e49d30f46297e3d7d531a06a0f7502c1
Peter Murray <peter-murray@users.noreply.github.com>
2020-12-16 14:56:38 +0000

    Adding debug

--- hue-lights-action/index.js
+++ hue-lights-action/index.js
@@ -37,11 +37,13 @@ function getLightNames() {
 }
 
 async function getLights(api, lightNames) {
+  console.log(`Looking for lights: ${JSON.stringify(lightNames)}`);
   if (!lightNames || lightNames.length > 0) {
     return Promise.resolve([]);
   }
 
   return api.lights.getAll().then(lights => {
+    console.log(JSON.stringify(lights));
     return lights.filter(light => {
       return lightNames.indexOf(light.name) > -1;
     });
